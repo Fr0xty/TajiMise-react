d10b1e89d9182ddcd5d87db40d92e0feb34edff2
Fr0xty <ongjiae8@gmail.com>
2022-06-11 14:34:22 +0800

    added profile fetch to represent user

--- src/components/AccountBar.tsx
+++ src/components/AccountBar.tsx
@@ -2,17 +2,36 @@ import '../styles/AccountBar.scss';
 
 import userIcon from '../assets/images/user_icon.svg';
 import cartIcon from '../assets/images/cart_icon.svg';
+import { useEffect, useState } from 'react';
 
 const AccountBar = () => {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [username, setUsername] = useState('loading...');
+    const [userPfp, setUserPfp] = useState(userIcon);
+
+    useEffect(() => {
+        (async () => {
+            if (document.cookie.includes('logged_in=')) {
+                setLoggedIn(true);
+
+                const fetchProfileReq = await fetch('/api/resource/profile');
+                const { name, profilePicture } = await fetchProfileReq.json();
+                setUsername(name);
+                if (profilePicture === null) return;
+                setUserPfp(profilePicture);
+            }
+        })();
+    }, []);
+
     return (
         <div className="account-bar">
-            <a href="/cart">
+            <a href={loggedIn ? '/cart' : '/login'}>
                 <img className="cart-icon selection" src={cartIcon} alt="shopping cart icon" />
             </a>
-            <a href="/account">
+            <a href={loggedIn ? '/account' : '/login'}>
                 <div className="account-navigation selection">
-                    <p className="account-name">Fr0xty</p>
-                    <img className="avatar-icon" src={userIcon} alt="avatar icon" />
+                    <p className="account-name">{loggedIn ? username : 'Log In'}</p>
+                    <img className="avatar-icon" src={userPfp} alt="avatar icon" />
                 </div>
             </a>
         </div>
