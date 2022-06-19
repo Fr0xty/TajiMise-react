bde94d29a6c1d12441310e2adf86fcdff7e2af9c
Fr0xty <ongjiae8@gmail.com>
2022-06-19 11:45:33 +0800

    hero component is responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,36 +1,19 @@
 import '../styles/Hero.scss';
-
 import Navbar from './Navbar';
-import { useEffect, useState } from 'react';
 
 const Hero = ({ currentPage }: { currentPage?: 'home' | 'about' | 'products' }) => {
-    const [loggedIn, setLoggedIn] = useState(false);
-
-    useEffect(() => {
-        if (document.cookie.includes('logged_in=')) setLoggedIn(true);
-    }, []);
-
     return (
         <div className="hero no-select">
             <Navbar currentPage={currentPage} noBackground={true} />
 
             <div className="content">
-                <div className="texts">
-                    <h1>
-                        Welcome To <br />
-                        <span>TajiMisé</span> Store!
-                    </h1>
-                    <p>
-                        Quality, Custom-Tailored Art is our raison d'être. <i>Anime</i>, <i>Manga</i> or <i>Game</i>{' '}
-                        characters you ruminate, why not bring them to reality?
-                    </p>
-                    <div className="action-row">
-                        <a href={loggedIn ? '/products' : '/login'}>
-                            <button>{loggedIn ? 'See Products' : 'Sign Up'}</button>
-                        </a>
-                        <pre>{loggedIn ? 'BROWSE OUR WIDE SELECTION!' : 'START YOUR JOURNEY!'}</pre>
-                    </div>
-                </div>
+                <h1>
+                    <span>TajiMisé</span> Store.
+                </h1>
+                <p>
+                    Quality, Custom-Tailored Art is our raison d'être. <i>Anime</i>, <i>Manga</i> or <i>Game</i>{' '}
+                    characters you ruminate, why not bring them to reality?
+                </p>
             </div>
         </div>
     );
