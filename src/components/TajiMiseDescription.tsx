ee829b59943b75280280356808638f9e93b229f0
Fr0xty <ongjiae8@gmail.com>
2022-06-20 18:23:48 +0800

    added description component for future wording of about page

---
+++ src/components/TajiMiseDescription.tsx
@@ -0,0 +1,11 @@
+import '../styles/TajiMiseDescription.scss';
+
+const TajiMiseDescription = () => {
+    return (
+        <div className="tajimise-description">
+            <h1>In Progress</h1>
+        </div>
+    );
+};
+
+export default TajiMiseDescription;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,11 @@
-import '../styles/About.scss';
 import Navbar from '../components/Navbar';
+import TajiMiseDescription from '../components/TajiMiseDescription';
 
 const About = () => {
     return (
         <div className="about">
             <Navbar currentPage="about" />
+            <TajiMiseDescription />
         </div>
     );
 };
