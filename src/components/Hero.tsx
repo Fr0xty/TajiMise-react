7f33b483353e95568ad923b5443427db969d4ea7
Fr0xty <ongjiae8@gmail.com>
2022-06-20 18:39:21 +0800

    styled hero samples

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import '../styles/Hero.scss';
 import sample1 from '../assets/images/sample1.png';
+import sample2 from '../assets/images/sample2.png';
 import Navbar from './Navbar';
 
 const Hero = ({ currentPage }: { currentPage?: 'home' | 'about' | 'products' }) => {
@@ -15,7 +16,11 @@ const Hero = ({ currentPage }: { currentPage?: 'home' | 'about' | 'products' }) 
                     Quality, Custom-Tailored Art is our raison d'être. <i>Anime</i>, <i>Manga</i> or <i>Game</i>{' '}
                     characters you ruminate, why not bring them to reality?
                 </p>
-                <img src={sample1} alt="sample" />
+
+                <div className="samples">
+                    <img src={sample1} className="sample-1" alt="sample 1" />
+                    <img src={sample2} className="sample-2" alt="sample 2" />
+                </div>
             </div>
         </div>
     );
