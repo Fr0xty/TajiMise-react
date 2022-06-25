a73a30bfc73cb40dc1eb49c3102cba6255a29ed8
Fr0xty <ongjiae8@gmail.com>
2022-06-25 15:43:39 +0800

    added Account page buttons function

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -3,13 +3,19 @@ import '../styles/Account.scss';
 import UserInfo from '../components/UserInfo';
 
 const Account = () => {
+    const LogoutEvent = async () => {};
+
     return (
         <div className="account">
             <div className="content-wrapper">
                 <UserInfo />
                 <div className="buttons no-select">
-                    <button className="home-btn">Go to Home</button>
-                    <button className="logout-btn">Log Out</button>
+                    <a href="/">
+                        <button className="home-btn">Go to Home</button>
+                    </a>
+                    <button className="logout-btn" onClick={LogoutEvent}>
+                        Log Out
+                    </button>
                 </div>
             </div>
         </div>
