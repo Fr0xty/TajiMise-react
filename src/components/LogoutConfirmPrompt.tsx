b090950fa5bdd727843be39ddfc2e80fdd33fa0a
Fr0xty <ongjiae8@gmail.com>
2022-06-25 16:01:56 +0800

    added LogoutConfirmPrompt component

---
+++ src/components/LogoutConfirmPrompt.tsx
@@ -0,0 +1,7 @@
+import '../styles/LogoutConfirmPrompt.scss';
+
+const LogoutConfirmPrompt = () => {
+    return <div className="logout-confirm-prompt"></div>;
+};
+
+export default LogoutConfirmPrompt;
--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,21 +1,25 @@
 import '../styles/Account.scss';
 
 import UserInfo from '../components/UserInfo';
+import LogoutConfirmPrompt from '../components/LogoutConfirmPrompt';
 
 const Account = () => {
     const LogoutEvent = async () => {};
 
     return (
-        <div className="account">
-            <div className="content-wrapper">
-                <UserInfo />
-                <div className="buttons no-select">
-                    <a href="/">
-                        <button className="home-btn">Go to Home</button>
-                    </a>
-                    <button className="logout-btn" onClick={LogoutEvent}>
-                        Log Out
-                    </button>
+        <div className="account-wrapper">
+            <LogoutConfirmPrompt />
+            <div className="account">
+                <div className="content-wrapper">
+                    <UserInfo />
+                    <div className="buttons no-select">
+                        <a href="/">
+                            <button className="home-btn">Go to Home</button>
+                        </a>
+                        <button className="logout-btn" onClick={LogoutEvent}>
+                            Log Out
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
