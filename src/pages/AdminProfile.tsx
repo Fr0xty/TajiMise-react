4f5f75e57ddbf63a306a452ffd4748ffd8d2f68c
Fr0xty <ongjiae8@gmail.com>
2022-06-28 18:19:17 +0800

    added AdminProfile page for admin info

---
+++ src/pages/AdminProfile.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+
+const AdminProfile = (adminHandle: string) => {
+    useEffect(() => {}, []);
+
+    return <div className="admin-profile"></div>;
+};
+
+export default AdminProfile;
