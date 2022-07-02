5401054a8217b2c9e3ea1418b104e8e89ef8a4bc
Fr0xty <ongjiae8@gmail.com>
2022-07-02 22:26:41 +0800

    made sell and orders page protected

--- src/pages/Sell.tsx
+++ src/pages/Sell.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 
 const Sell = () => {
+    useEffect(() => {
+        (async () => {
+            /**
+             * redirect to /products if user is not admin
+             */
+            if (!document.cookie.includes('logged_in=')) return (document.location.href = '/products');
+
+            const isAdminReq = await fetch('/api/resource/is-admin');
+            if (isAdminReq.status !== 200) return;
+
+            const isAdmin = await isAdminReq.text();
+            if (isAdmin === 'false') document.location.href = '/products';
+        })();
+    }, []);
+
     return (
         <div className="sell">
             <Navbar />
