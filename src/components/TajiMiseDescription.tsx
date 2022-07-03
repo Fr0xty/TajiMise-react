e2c5e4210be8d88650ae9bf079fc4845edb0256f
Fr0xty <ongjiae8@gmail.com>
2022-07-03 12:36:50 +0800

    added simple about (will change in the future)

--- src/components/TajiMiseDescription.tsx
+++ src/components/TajiMiseDescription.tsx
@@ -2,8 +2,28 @@ import '../styles/TajiMiseDescription.scss';
 
 const TajiMiseDescription = () => {
     return (
-        <div className="tajimise-description">
-            <h1>In Progress</h1>
+        <div className="tajimise-description-wrapper">
+            <div className="background" />
+
+            <div className="tajimise-description">
+                <h1>What do we do?</h1>
+                <p>We sell all things containing illustration like:</p>
+                <ul>
+                    <li>Physical & Digital Drawings</li>
+                    <li>Bookmarks</li>
+                    <li>Keychains</li>
+                    <li>etc..</li>
+                </ul>
+                <p>
+                    We are commited to provide only hand-drawn & custom-tailored art. That means no printing to mass
+                    produce and characters can be requested as a basic service for no extra charge.
+                </p>
+                <br />
+                <p>
+                    For now products on here is limited but we will be taking orders online soon. We appreciate for your
+                    continued support and patronage.
+                </p>
+            </div>
         </div>
     );
 };
