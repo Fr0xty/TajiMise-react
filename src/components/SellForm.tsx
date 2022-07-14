3a048aff1ace46e3219d4491c9d2b2532b8f112d
Fr0xty <ongjiae8@gmail.com>
2022-07-14 18:42:05 +0800

    removed unused files to keep down bundle size

--- src/components/SellForm.tsx
+++
@@ -1,7 +0,0 @@
-import '../styles/SellForm.scss';
-
-const SellForm = () => {
-    return <div className="sell-form"></div>;
-};
-
-export default SellForm;
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,29 +1,12 @@
-import { useEffect, useState } from 'react';
 import AccountBar from '../components/AccountBar';
-import AdminProductNav from '../components/AdminProductNav';
 import Navbar from '../components/Navbar';
 import ProductSelect from '../components/ProductSelect';
 
 const Products = () => {
-    const [isAdmin, setIsAdmin] = useState(false);
-
-    useEffect(() => {
-        (async () => {
-            if (!document.cookie.includes('logged_in=')) return;
-
-            const isAdminReq = await fetch('/api/resource/is-admin');
-            if (isAdminReq.status !== 200) return;
-
-            const isAdminResult = await isAdminReq.text();
-            if (isAdminResult === 'true') setIsAdmin(true);
-        })();
-    });
-
     return (
         <div className="products no-select">
             <Navbar currentPage="products" noAccountNavs />
             <AccountBar />
-            {isAdmin && <AdminProductNav />}
             <ProductSelect />
         </div>
     );
