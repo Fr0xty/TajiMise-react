95c6c7509b83003e0155b674faa602707f2b9a9e
Fr0xty <ongjiae8@gmail.com>
2022-07-14 18:10:39 +0800

    vastly improved AdminCard responsivity

--- src/components/BreifAbout.tsx
+++ src/components/BreifAbout.tsx
@@ -11,44 +11,46 @@ const BreifAbout = () => {
         <div className="breif-about">
             <h1 className="home-main-header">Who run TajiMisé?</h1>
 
-            <div className="cards">
-                <AdminCard
-                    avatar={tameijiAvatar}
-                    description="General Management of the company and Artist for our
+            <div className="cards-wrapper">
+                <div className="cards">
+                    <AdminCard
+                        avatar={tameijiAvatar}
+                        description="General Management of the company and Artist for our
                             products."
-                    name="Tajima Eiji (Tameiji)"
-                    position="CEO & Artist"
-                    pronouns="he / him"
-                    handle="tameiji"
-                    className="tameiji-card"
-                />
-                <AdminCard
-                    avatar={capirinAvatar}
-                    description="Artist who fullfils our products."
-                    name="capirin"
-                    position="Artist"
-                    pronouns="he / him"
-                    handle="capirin"
-                    className="capirin-card"
-                />
-                <AdminCard
-                    avatar={fr0xtyAvatar}
-                    description="Handles any technology-related tasks / Lead Developer and Primary Designer."
-                    name="Fr0xty"
-                    position="IoT Engineer & Graphics Designer"
-                    pronouns="they / them / any"
-                    handle="fr0xty"
-                    className="fr0xty-card"
-                />
-                <AdminCard
-                    avatar={arataAvatar}
-                    description="Helps with the finances of the company by collecting, tracking, and auditing cashflow."
-                    name="Arata"
-                    position="Accountant"
-                    pronouns="they / them / it"
-                    handle="arata"
-                    className="arata-card"
-                />
+                        name="Tajima Eiji (Tameiji)"
+                        position="CEO & Artist"
+                        pronouns="he / him"
+                        handle="tameiji"
+                        className="tameiji-card"
+                    />
+                    <AdminCard
+                        avatar={capirinAvatar}
+                        description="Artist who fullfils our products."
+                        name="capirin"
+                        position="Artist"
+                        pronouns="he / him"
+                        handle="capirin"
+                        className="capirin-card"
+                    />
+                    <AdminCard
+                        avatar={fr0xtyAvatar}
+                        description="Handles any technology-related tasks / Lead Developer and Primary Designer."
+                        name="Fr0xty"
+                        position="IoT Engineer & Graphics Designer"
+                        pronouns="they / them / any"
+                        handle="fr0xty"
+                        className="fr0xty-card"
+                    />
+                    <AdminCard
+                        avatar={arataAvatar}
+                        description="Helps with the finances of the company by collecting, tracking, and auditing cashflow."
+                        name="Arata"
+                        position="Accountant"
+                        pronouns="they / them / it"
+                        handle="arata"
+                        className="arata-card"
+                    />
+                </div>
             </div>
         </div>
     );
